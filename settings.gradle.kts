a384797210ea7b7f21f4e5e11726611edc266a83
mgroth0 <mgroth49@gmail.com>
2023-02-04 14:10:27 -0500

    autocommit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,7 +35,7 @@ return "$group:$name:$version"
 }
 val depsSeen  = mutableListOf<Dep>()
 listOf("kbuild").forEach { gradleMod ->
-val kbuildVersion  = "1675537748976"
+val kbuildVersion  = "1675537820686"
 val mattCacheFolder  = rootDir.resolve(".gradle").resolve("matt")
 val downloadedKbuildVersionFile  = mattCacheFolder.resolve("kbuildVersion.txt")
 val kbuildLibsFolder  = mattCacheFolder.resolve("lib")
