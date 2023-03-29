a747f0e1d1cb4486519e0b72ffbfadb9bc707eb2
mgroth0 <mgroth49@gmail.com>
2023-03-29 16:28:23 -0400

    autocommit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -100,8 +100,7 @@ buildscript {
             }
         }
 
-        realPluginsList.filter { it.name.isNotBlank() }.forEach { gradleMod ->
-            println("gradleMod: ${gradleMod.name}")
+        realPluginsList.forEach { gradleMod ->
             val kbuildLibsFolder = if (gradleMod.version == null) {
                 val kbuildDir = registeredDir.resolve("gbuild/dist/${gradleMod.name}")
                 val numBack = prop("NUM_BACK").toInt()
